d1c6d817492c5af80f041e5fef2bbaf451b798a0
MonicaTohe <Contacto.MonicaHf@gmail.com>
2024-11-25 00:03:39 -0300

    ahorasi

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,12 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs"); // <- IMPORTAR BCRYPTJS
+const bcrypt = require("bcryptjs"); 
 
 exports.registerUser = async (req, res) => {
   const { name, email, password } = req.body;
   try {
-    const hashedPassword = await bcrypt.hash(password, 10); // Encripta la contraseña antes de guardarla
-    const user = await User.create({ name, email, password: hashedPassword }); // Guarda la contraseña encriptada
+    const hashedPassword = await bcrypt.hash(password, 10); 
+    const user = await User.create({ name, email, password: hashedPassword });
     res.status(201).json({ message: "Usuario registrado", user });
   } catch (error) {
     res.status(400).json({ message: "Error al registrar usuario" });
@@ -17,9 +17,9 @@ exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (user && (await bcrypt.compare(password, user.password))) {
-    // Compara la contraseña encriptada
+   
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h", // Opcional: expira en 1 hora
+      expiresIn: "1h", 
     });
     res.json({ token });
   } else {
@@ -28,7 +28,7 @@ exports.loginUser = async (req, res) => {
 };
 
 exports.verifyToken = (req, res) => {
-  const token = req.headers.authorization?.split(" ")[1]; // Asegúrate de obtener solo el token (después de "Bearer")
+  const token = req.headers.authorization?.split(" ")[1]; 
   if (!token) return res.status(401).json({ message: "Token requerido" });
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/product", productRoutes);
 
-// Ruta raíz para comprobar que el servidor funciona
+
 app.get("/", (req, res) => {
   res.send("¡Servidor funcionando correctamente!");
 });
